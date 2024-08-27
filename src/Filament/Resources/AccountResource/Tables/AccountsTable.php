758da2e8a07219422bd8cbef310b2bee5e1db9b5
Fady Mondy <info@3x1.io>
2024-08-27 17:02:12 +0300

    drop uncomplate feature of export

--- src/Filament/Resources/AccountResource/Tables/AccountsTable.php
+++ src/Filament/Resources/AccountResource/Tables/AccountsTable.php
@@ -117,38 +117,38 @@ class AccountsTable extends TableBuilder
                 ->toggleable(isToggledHiddenByDefault: true),
         ]);
         return $table
-            ->headerActions([
-                Tables\Actions\Action::make('export')
-                    ->requiresConfirmation()
-                    ->color('info')
-                    ->icon('heroicon-o-arrow-down-on-square')
-                    ->fillForm([
-                        'columns' => [
-                            'id' => trans('filament-accounts::messages.accounts.coulmns.id'),
-                            'name' => trans('filament-accounts::messages.accounts.coulmns.name'),
-                            'email' => trans('filament-accounts::messages.accounts.coulmns.email'),
-                            'phone' => trans('filament-accounts::messages.accounts.coulmns.phone'),
-                            'address' => trans('filament-accounts::messages.accounts.coulmns.address'),
-                            'type' => trans('filament-accounts::messages.accounts.coulmns.type'),
-                            'is_login' => trans('filament-accounts::messages.accounts.coulmns.is_login'),
-                            'is_active' => trans('filament-accounts::messages.accounts.coulmns.is_active'),
-                            'created_at' => trans('filament-accounts::messages.accounts.coulmns.created_at'),
-                            'updated_at' => trans('filament-accounts::messages.accounts.coulmns.updated_at'),
-                        ]
-                    ])
-                    ->form([
-                        Forms\Components\KeyValue::make('columns')
-                            ->required()
-                            ->editableKeys(false)
-                            ->addable(false)
-                    ])
-                    ->action(function (array $data){
-                        return Excel::download(new ExportAccounts($data), 'accounts.csv');
-                    }),
-                Tables\Actions\Action::make('import')
-                    ->color('warning')
-                    ->icon('heroicon-o-arrow-up-on-square'),
-            ])
+//            ->headerActions([
+//                Tables\Actions\Action::make('export')
+//                    ->requiresConfirmation()
+//                    ->color('info')
+//                    ->icon('heroicon-o-arrow-down-on-square')
+//                    ->fillForm([
+//                        'columns' => [
+//                            'id' => trans('filament-accounts::messages.accounts.coulmns.id'),
+//                            'name' => trans('filament-accounts::messages.accounts.coulmns.name'),
+//                            'email' => trans('filament-accounts::messages.accounts.coulmns.email'),
+//                            'phone' => trans('filament-accounts::messages.accounts.coulmns.phone'),
+//                            'address' => trans('filament-accounts::messages.accounts.coulmns.address'),
+//                            'type' => trans('filament-accounts::messages.accounts.coulmns.type'),
+//                            'is_login' => trans('filament-accounts::messages.accounts.coulmns.is_login'),
+//                            'is_active' => trans('filament-accounts::messages.accounts.coulmns.is_active'),
+//                            'created_at' => trans('filament-accounts::messages.accounts.coulmns.created_at'),
+//                            'updated_at' => trans('filament-accounts::messages.accounts.coulmns.updated_at'),
+//                        ]
+//                    ])
+//                    ->form([
+//                        Forms\Components\KeyValue::make('columns')
+//                            ->required()
+//                            ->editableKeys(false)
+//                            ->addable(false)
+//                    ])
+//                    ->action(function (array $data){
+//                        return Excel::download(new ExportAccounts($data), 'accounts.csv');
+//                    }),
+//                Tables\Actions\Action::make('import')
+//                    ->color('warning')
+//                    ->icon('heroicon-o-arrow-up-on-square'),
+//            ])
             ->columns($colums)
             ->filters(AccountsFilters::make())
             ->actions(AccountsActions::make())
